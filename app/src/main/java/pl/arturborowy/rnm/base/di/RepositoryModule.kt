f0ae955836d1335ce65ea327b7a80c64f727f105
Artur Borowy <artur.borowy843@gmail.com>
2020-09-20 17:31:11 +0200

    Create StationsCacheMetadataLocalRepository.

--- app/src/main/java/pl/arturborowy/rnm/base/di/RepositoryModule.kt
+++ app/src/main/java/pl/arturborowy/rnm/base/di/RepositoryModule.kt
@@ -1,12 +1,22 @@
 package pl.arturborowy.rnm.base.di
 
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import pl.arturborowy.rnm.base.date.YearMonthDayHourMinuteSecondMillisDateConverter
 import pl.arturborowy.rnm.data.local.stations.cache.StationsRoomRepository
+import pl.arturborowy.rnm.data.local.stations.cache.meta.StationsCacheMetadataSpRepository
 import pl.arturborowy.rnm.data.remote.stations.StationsServiceRemoteRepository
 import pl.arturborowy.rnm.domain.stations.StationsLocalRepository
 import pl.arturborowy.rnm.domain.stations.StationsRemoteRepository
+import pl.arturborowy.rnm.domain.stations.meta.StationsCacheMetadataLocalRepository
 
 val repositoryModule = module {
     single<StationsRemoteRepository> { StationsServiceRemoteRepository(get(), get(), get()) }
     single<StationsLocalRepository> { StationsRoomRepository(get(), get(), get(), get(), get()) }
+    single<StationsCacheMetadataLocalRepository> {
+        StationsCacheMetadataSpRepository(
+            get(),
+            get(named<YearMonthDayHourMinuteSecondMillisDateConverter>())
+        )
+    }
 }
---
+++ app/src/main/java/pl/arturborowy/rnm/domain/stations/meta/StationsCacheMetadataLocalRepository.kt
@@ -0,0 +1,8 @@
+package pl.arturborowy.rnm.domain.stations.meta
+
+import java.util.*
+
+interface StationsCacheMetadataLocalRepository {
+
+    val cacheCreationTime: Date?
+}
