07991b3970bf5d9619de91de6d35286ff4ca3c0a
Artur Borowy <artur.borowy843@gmail.com>
2020-09-20 14:43:21 +0200

    Add loading overlay.

--- app/src/main/java/pl/arturborowy/rnm/stationssearch/StationsSearchViewModel.kt
+++ app/src/main/java/pl/arturborowy/rnm/stationssearch/StationsSearchViewModel.kt
@@ -6,6 +6,7 @@ import io.reactivex.rxkotlin.subscribeBy
 import pl.arturborowy.rnm.base.error.ThrowableHandler
 import pl.arturborowy.rnm.base.rx.SchedulerProvider
 import pl.arturborowy.rnm.base.rx.setSchedulers
+import pl.arturborowy.rnm.base.ui.view.LoadingScreenViewModel
 import pl.arturborowy.rnm.base.ui.viewmodel.FragmentViewModel
 import pl.arturborowy.rnm.base.ui.viewmodel.RxJavaSubscriber
 import pl.arturborowy.rnm.domain.stations.StationsInteractor
@@ -16,7 +17,8 @@ class StationsSearchViewModel(
     private val stationsInteractor: StationsInteractor,
     override val disposables: CompositeDisposable,
     private val schedulerProvider: SchedulerProvider,
-    private val throwableHandler: ThrowableHandler
+    private val throwableHandler: ThrowableHandler,
+     val loadingScreenViewModel: LoadingScreenViewModel
 ) : FragmentViewModel(), RxJavaSubscriber {
 
     val stations = ObservableField<MutableList<StationEntity>>(mutableListOf())
@@ -24,9 +26,15 @@ class StationsSearchViewModel(
 
     override fun onViewCreated() {
         super.onViewCreated()
+        fetchStationsAndKeywords()
+    }
+
+    private fun fetchStationsAndKeywords() {
+        loadingScreenViewModel.show()
 
         stationsInteractor.getStationsAndKeywords()
             .setSchedulers(schedulerProvider)
+            .doAfterTerminate(loadingScreenViewModel::hide)
             .subscribeBy(
                 onSuccess = { (stations, keywords) ->
                     this.keywords.set(keywords.toMutableList())
